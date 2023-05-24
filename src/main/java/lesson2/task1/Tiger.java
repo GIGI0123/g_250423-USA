19497ec938a0df0a48a0cf95cb2e58193d553adf
Zhiide Dzhuzumkulova <zhiidedzhuzumkulova@Zhiides-iMac.attlocal.net>
2023-05-23 21:45:22 -0400

    Initial commit

---
+++ src/main/java/lesson2/task1/Tiger.java
@@ -0,0 +1,12 @@
+package lesson2.task1;
+
+public class Tiger {
+
+     public int weight;
+    static int legsCount = 4;
+    public void sleep() {
+        System.out.println("Tiger sleep");
+        System.out.println(weight);
+
+    }
+}
---
+++ src/main/java/lesson2/task2/Main.java
@@ -0,0 +1,13 @@
+package lesson2.task2;
+
+import lesson2.task1.Tiger;
+
+public class main {
+    package lesson2.task2;
+    import lesson2.task1.Tiger;
+    public class Main {
+        public void () {
+            Tiger tiger = new Tiger();
+            tiger.weight = 100;
+
+}
